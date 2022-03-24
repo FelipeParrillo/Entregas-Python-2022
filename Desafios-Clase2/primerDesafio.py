3b3dfd63d28dfed314cd99f42a11d381c302fc9b
FelipeParrillo <62100348+FelipeParrillo@users.noreply.github.com>
2022-03-24 01:38:58 -0300

    Update primerDesafio.py

--- Desafios-Clase2/primerDesafio.py
+++ Desafios-Clase2/primerDesafio.py
@@ -1,6 +1,6 @@
 """Vamos a modificar el código anterior para que se imprima la cadena “TIENE
 R” si la palabra contiene la letra r y sino, imprima “NO TIENE R”.
 """
-for i in range(4):
-    cadena=input('Ingresa la cadena: ')
-    print("TIENE R") if 'r' in (cadena) else print("NO TIENE R")
+for x in range(4):
+    str=input('Ingresa la cadena: ')
+    print("TIENE R") if 'r' in (str) else print("NO TIENE R")
