935c3c47c4075dd15c5d257b2cf80021fbb3c864
FelipeParrillo <felaypo33@gmail.com>
2022-03-23 23:25:50 -0300

    "primer desafio"

---
+++ Desafios-Clase1/primerDesafio.py
@@ -0,0 +1,9 @@
+"""• Queremos ingresar un número desde el teclado e imprimir si el número es o no par.
+   • ¿Cómo sería el pseudocódigo de esto?"""
+
+
+num=int(input('Ingresa tu numero: '))
+if num %2==0:
+    print(f'El numero: {num} es par')
+else:
+    print(f'El numero: {num} no es par')    
