05871b18306af6890917184021e83b1a3dbac9e5
FelipeParrillo <felaypo33@gmail.com>
2022-04-03 18:10:15 -0300

    commit 4/3/22

---
+++ TrabajoPractico2/Ej11.py
@@ -0,0 +1,40 @@
+def duped(archN1, archN2):
+    '''Retorna una lista con los nombres duplicados de ambas strings '''
+    nombres1 = archN1.replace(',', '').replace("'", "").split()
+    nombres2 = archN2.replace(',', '').replace("'", "").split()
+
+    dup = [x for x in nombres1 if x in nombres2]
+    return dup
+
+def Imprimir(archN1, archP1, archP2):
+    ''' Imprime en Columnas los datos de cada String pasado'''
+    nombres = archN1.replace(',','').replace("'","").lower().split()
+    p1 = archP1.replace(',','').split()
+    p2 = archP2.replace(',','').split()
+    
+
+    
+
+    print(' | Nombre | Nota1 | Nota2 | Total')
+    for n in range(len(nombres)):
+        print(f'{n} | {nombres[n]} | {p1[n]} | {p2[n]} | {(int(p1[n]) + int(p2[n]))}')
+
+
+
+
+
+
+# Abro los archivos y los paso a strings
+with open("Python-Actividades\\Entregas-Python-2022\\TrabajoPractico2\\nombres_1.txt", encoding="utf8") as archivo_nombres1:
+    archN1 = archivo_nombres1.read()
+with open("Python-Actividades\\Entregas-Python-2022\\TrabajoPractico2\\nombres_2.txt", encoding="utf8") as archivo_nombres2:
+    archN2 = archivo_nombres2.read()
+with open("Python-Actividades\\Entregas-Python-2022\\TrabajoPractico2\\eval1.txt", encoding='utf8') as archivo_prueba1:
+    archP1 = archivo_prueba1.read()
+with open("Python-Actividades\\Entregas-Python-2022\\TrabajoPractico2\\eval2.txt", encoding='utf8') as archivo_prueba2:
+    archP2 = archivo_prueba2.read()
+
+dup = duped(archN1, archN2)
+print(f'Los nombres que se encunetran en ambos son: {dup}')
+
+Imprimir(archN1, archP1, archP2)
--- TrabajoPractico2/Ej9.py
+++ TrabajoPractico2/Ej9.py
@@ -17,11 +17,12 @@ anterior, sería:
 ]
 """
 
-           
+
 estructura = ['-*-*-',
               '--*--',
               '----*',
               '*----']
+
 
 def crearMatriz(matriz):
     """Agarra la la lista 'estructura' y convierte cada string dentro en otra lista para convertirla en una matriz y facilitar su recorrido"""
@@ -31,39 +32,40 @@ def crearMatriz(matriz):
 
 def recorrerMatriz(matriz):
     """Recorre la matriz revisando todos los posibles existentes puntos al rededor de la posicion a evaluar y suma al contador cuando hay una bomba"""
-    for x in  range(len(matriz)):
+    for x in range(len(matriz)):
         for y in range(len(matriz[x])):
-            #verifico que la pos a evaluar es una casilla libre de bomba
-            if (matriz[x][y] == '-'): 
-                cont=0
+            # verifico que la pos a evaluar es una casilla libre de bomba
+            if (matriz[x][y] == '-'):
+                cont = 0
                 # hago un try al rededor de la pos a evaluar por si existe un index out of range
                 try:
-                    if matriz[x-1][y-1]=='*':
-                        cont+=1
-                    if matriz[x][y-1]=='*':
-                        cont+=1
-                    if matriz[x+1][y-1]=='*':
-                        cont+=1
-                    if matriz[x+1][y]=='*':
-                        cont+=1
-                    if matriz[x+1][y+1]=='*':
-                        cont+=1
-                    if matriz[x][y+1]=='*':
-                        cont+=1
-                    if matriz[x-1][y+1]=='*':
-                        cont+=1
-                    if matriz[x-1][y]=='*':
-                        cont+=1
-                    
-                except: print(f'this position  matriz[{x}][{y}] does not exist (index out of range)')
-                matriz[x][y]=cont
-    
+                    if matriz[x-1][y-1] == '*':
+                        cont += 1
+                    if matriz[x][y-1] == '*':
+                        cont += 1
+                    if matriz[x+1][y-1] == '*':
+                        cont += 1
+                    if matriz[x+1][y] == '*':
+                        cont += 1
+                    if matriz[x+1][y+1] == '*':
+                        cont += 1
+                    if matriz[x][y+1] == '*':
+                        cont += 1
+                    if matriz[x-1][y+1] == '*':
+                        cont += 1
+                    if matriz[x-1][y] == '*':
+                        cont += 1
+
+                except:
+                    print(
+                        f'this position  matriz[{x}][{y}] does not exist (index out of range)')
+                matriz[x][y] = cont
+
     for n in range(len(matriz)):
-        matriz[n]=matriz[n]
+        matriz[n] = matriz[n]
         print(matriz[n])
 
 
-                
-matriz=[]
+matriz = []
 crearMatriz(matriz)
 recorrerMatriz(matriz)
